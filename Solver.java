f8fb9f5efcc9879f98d41b1c5a0ee441f1ede110
Aksel <alexmagda321@gmail.com>
2024-11-23 23:15:09 +0100

    pridanie kontolu validity

--- Solver.java
+++ Solver.java
@@ -1,11 +1,7 @@
 public class Solver {
-    private final int GRID_SIZE = 9;
-
     // Overí, či sa číslo nachádza v danom riadku.
-    private boolean isNumberInRow(int value, int row) {
-        Grid grid = new Grid();
-
-        for (int index = 0; index < this.GRID_SIZE; index++) {
+    private boolean isNumberInRow(Grid grid, int value, int row) {
+        for (int index = 0; index < 9; index++) {
             if (value == grid.getPlayersWholeSudoku()[row][index]) {
                 return true; // Nachádza sa.
             }
@@ -15,10 +11,8 @@ public class Solver {
     }
 
     // Overí, či sa číslo nachádza v danom stĺpci.
-    private boolean isNumberInColumn(int value, int col) {
-        Grid grid = new Grid();
-
-        for (int index = 0; index < this.GRID_SIZE; index++) {
+    private boolean isNumberInColumn(Grid grid, int value, int col) {
+        for (int index = 0; index < 9; index++) {
             if (value == grid.getPlayersWholeSudoku()[index][col]) {
                 return true; // Nachádza sa.
             }
@@ -28,14 +22,12 @@ public class Solver {
     }
 
     // Overí, či sa číslo nachádza v danom bloku.
-    private boolean isNumberInBox(int value, int row, int col) {
-        Grid grid = new Grid();
-
+    private boolean isNumberInBox(Grid grid, int value, int row, int col) {
         int[][] playersSudoku = grid.getPlayersWholeSudoku();
         int x = (row / 3) * 3; // Začiatok bloku v riadku.
         int y = (col / 3) * 3; // Začiatok blocu v stĺpci.
 
-        // Prehladá 3x3 blok
+        // Prehladá 3x3 blok.
         for (int col_index = 0; col_index < 3; col_index++) {
             for (int row_index = 0; row_index < 3; row_index++) {
                 if (value == playersSudoku[x + col_index][y + row_index]) {
@@ -47,12 +39,22 @@ public class Solver {
         return false; // Nenachádza sa v bloku.
     }
 
+    private boolean isValid(Grid grid, int value, int row, int col) {
+        return !isNumberInRow(grid, value, row) &&
+                !isNumberInColumn(grid, value, col) &&
+                !isNumberInBox(grid, value, row, col);
+    }
+
+    private void solve(Grid grid, int value, int row, int col) {
+
+    }
+
 
 
     public static void main(String[] args) {
+        Grid grid = new Grid();
         Solver idk = new Solver();
-        System.out.println(idk.isNumberInRow(4, 8));
-        System.out.println(idk.isNumberInColumn(9, 5));
-        System.out.println(idk.isNumberInBox(6, 3, 6));
+
+        System.out.println(idk.isValid(grid, 9, 1, 1));
     }
 }
